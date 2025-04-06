5c3672701b277121e0b6e30028e9b575c990ab9c
vikabelova <belova.viktoriya@gmail.com>
2025-04-06 16:29:40 +0700

    New commit

--- src/test/java/ru/netology/OrderTest.java
+++ src/test/java/ru/netology/OrderTest.java
@@ -10,6 +10,8 @@ import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
 import org.openqa.selenium.chrome.ChromeOptions;
 
+import java.time.Duration;
+
 import static org.junit.jupiter.api.Assertions.assertEquals;
 
 public class OrderTest {
@@ -18,17 +20,19 @@ public class OrderTest {
 
     @BeforeAll
     static void setupAll() {
-        WebDriverManager.chromedriver().setup();
-        System.out.println("ChromeDriver version: " + WebDriverManager.chromedriver().getDownloadedDriverVersion ());
+        WebDriverManager.chromedriver().clearDriverCache().setup();
     }
 
     @BeforeEach
     void setUp() {
         ChromeOptions options = new ChromeOptions();
-        options.addArguments("--disable-dev-shm-usage");
-        options.addArguments("--no-sandbox");
-        //options.addArguments("--headless");
+        options.addArguments(
+                "--start-maximized",
+                "--disable-extensions",
+                "--remote-allow-origins=*"
+        );
         driver = new ChromeDriver(options);
+        driver.manage().timeouts().implicitlyWait(Duration.ofSeconds(10));
         driver.get("http://localhost:9999");
     }
 
@@ -46,16 +50,6 @@ public class OrderTest {
         driver.findElement(By.cssSelector("button[type=button]")).click();
         String actualText = driver.findElement(By.cssSelector("[data-test-id=order-success]")).getText().trim();
         assertEquals("Ваша заявка успешно отправлена! Наш менеджер свяжется с вами в ближайшее время.", actualText);
-    }
-
-    @Test
-    void InvalidNameSurnameTest1() {
-        driver.findElement(By.cssSelector("[type='text']")).sendKeys("Ivanov Ivan");
-        driver.findElement(By.cssSelector("[type='tel']")).sendKeys("+79991234567");
-        driver.findElement(By.cssSelector("[data-test-id='agreement']")).click();
-        driver.findElement(By.cssSelector(".button__text")).click();
-        String text = driver.findElement(By.cssSelector("[data-test-id='name'].input_invalid .input__sub")).getText();
-        assertEquals("Имя и Фамилия указаные неверно. Допустимы только русские буквы, пробелы и дефисы.", text.trim());
     }
 
 }
